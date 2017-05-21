9187f766e7124a80e9360f19d9022c3e210f6f2c
Onsel Akin <onsela@outlook.com>
2017-05-21 19:12:41 +0300

    Fixed the bug that prevented Assign action from resolving it's promise.

--- src/engine/actions/assign.ts
+++ src/engine/actions/assign.ts
@@ -11,13 +11,14 @@ export class AssignAction extends Action {
             if (_.indexOf(this.scenario.globalVariableNames, this.to) > -1) {
                 this.scenario[this.to] = this.value;
                 await this.scenario.cacheService.set(this.to, this.value);
-                resolve();
+                return resolve();
             }
             if (_.indexOf(this.scenario.actorVariableNames, this.to) > -1) {
                 this.scenario[this.to] = this.value;
                 await this.scenario.cacheService.set(`${this.scenario.uniqueId}_${this.to}`, this.value);
-                resolve();
+                return resolve();
             }
+            resolve();
         });
     }
 }
